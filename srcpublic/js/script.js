21c3ee779e1d7fb81f7717dbe99658fdd9875dfb
AdiSan103 <69831671+AdiSan103@users.noreply.github.com>
2022-04-01 09:06:09 +0800

    updated

---
+++ srcpublic/js/script.js
@@ -0,0 +1,89 @@
+var swiper = new Swiper(".banner", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
+
+var swiper = new Swiper(".content3__slider", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+  },
+  });
+
+  var swiper = new Swiper(".gallery__main", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+  },
+  });
+
+
+  var swiper = new Swiper(".testimoni__main", {
+    effect: "cards",
+    grabCursor: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+  },
+  });
+
+  // hamburger-menu
+function hamburgerMenu() {
+  if (document.querySelector('.nav__main').classList.contains('d-none')) {
+      document.querySelector('.nav__main').classList.remove('d-none');
+  } else {
+      document.querySelector('.nav__main').classList.add('d-none');
+  }
+}
+
+ // hamburger-menu
+window.addEventListener("scroll", () => {
+  let thisScroll = this.scrollY;
+  if(thisScroll > document.getElementsByClassName('nav__start')[0].offsetTop) {
+      document.getElementsByClassName('nav__main')[0].classList.add("nav__up");
+  }
+});
+
+// aos js
+AOS.init();
